180360a0206d34a5a751077ad6201e7d57004fe2
dp160610 <46530061+dp160610@users.noreply.github.com>
2022-07-19 13:51:47 +0530

    detai;s of hooks

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,43 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+  const reset = () => {
+    setValue(0);
+  }
+
+const comlpexIncrease = () => {
+  setTimeout(() => {
+    // setValue(value + 1)
+    setValue((prevState) => {
+     return prevState + 1;
+    })
+  }, 2000)
+}
+  return (
+    <>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>Regular Counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={() => setValue(value - 1)}>
+          Decrease
+        </button>
+        <button className="btn" onClick={() => setValue(value + 1)}>
+          Increase
+        </button>
+        <button className="btn" onClick={reset}>
+          Reset
+        </button>
+      </section>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>more complex Counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={comlpexIncrease}>
+          Increase Later
+        </button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+  <h1>Value: {secondValue}</h1> */}
+      <h1>{text || "Durga Prasad"}</h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {isError && <h1>Error...</h1>}
+      {isError ? (
+        <p>there is error...</p>
+      ) : (
+        <div>
+          <h2>There is no error.</h2>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ShortCircuit;
